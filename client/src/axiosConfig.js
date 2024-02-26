4c8438ce2b327cdc8de39afd1df5cde21017860a
ousman-hedir <moreinfo203@gmail.com>
2024-02-26 19:04:56 +0300

    front and backend codes

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-	baseURL: "http://localhost:3309/api",
+	// baseURL: "http://localhost:3309/api",
+	baseURL: "https://group-forum.onrender.com/api",
 });
 export default axiosBase;
